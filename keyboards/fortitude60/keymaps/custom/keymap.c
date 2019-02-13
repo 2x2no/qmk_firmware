ebb6586361ef38a5367a9e81995c837f864491e5
shinomiya <shinomiya@synchro-food.co.jp>
2019-02-13 12:31:12 +0900

    fortitude60キーマップ変更

--- keyboards/fortitude60/keymaps/custom/keymap.c
+++ keyboards/fortitude60/keymaps/custom/keymap.c
@@ -39,17 +39,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
  * |      |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  | Esc  | Bksp |   N  |   M  |   ,  |   .  |   /  |  \   |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  | Esc  | Del  |   N  |   M  |   ,  |   .  |   /  |  \   |
  * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
- *               | Ctrl |  Alt | LGUI |Space |Lower |Raise |Enter | RGUI |  [   |   ]  |
+ *               | Ctrl |  Alt | LGUI |Space |Lower | Bksp |Enter |Raise |  [   |   ]  |
  *               `---------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
   _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ESC,   KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS , \
-                    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  LOWER,    RAISE,   KC_ENT,   KC_RGUI, KC_LBRC, KC_RBRC\
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ESC,   KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS , \
+                    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  LOWER,    KC_BSPC, KC_ENT,  RAISE,   KC_LBRC, KC_RBRC\
 ),
 
 /* Colemak
@@ -102,17 +102,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
  * |      |      |      |      |      |      |             | Bksp | Left | Down | Right|      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      | Del　 |Enter |      |      |      |      |      |
+ * |      |      |      |      |      |      |      |      |Enter |      |      |      |      |      |
  * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
- *               |      |      |      |      |      |      |      |      |      |      |
+ *               |      |      |      |      |      | Del  |      |      |      |      |
  *               `---------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT( \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
   _______, _______, _______, _______, _______, _______,                   KC_DEL,  _______, KC_UP,   _______, _______, KC_F12, \
   _______, _______, _______, _______, _______, _______,                   KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, KC_DEL,  KC_ENT,  _______, _______, _______, _______, _______, \
-                    _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______\
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_ENT,  _______, _______, _______, _______, _______, \
+                    _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______\
 ),
 
 /* Raise
@@ -123,17 +123,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
  * |      |      |      |      |      |      |             |      | Left | Down |Right |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      | Del　 |Enter |      |      |      |      |      |
+ * |      |      |      |      |      |      |      |      |Enter |      |      |      |      |      |
  * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
- *               |      |      |      |      |      |      |      |      |      |      |
+ *               |      |      |      |      |      |  Del |      |      |      |      |
  *               `---------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT( \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
   _______, _______, _______, _______, _______, _______,                   _______,  _______, KC_UP,   _______, _______, KC_F12, \
   _______, _______, _______, _______, _______, _______,                   _______,  KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, KC_DEL,  KC_ENT,   _______, _______, _______, _______, _______, \
-                    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______\
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_ENT,   _______, _______, _______, _______, _______, \
+                    _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______\
 ),
 
 /* Adjust (Lower + Raise)
@@ -164,26 +164,64 @@ uint32_t layer_state_set_user(uint32_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
+static bool lower_pressed = false;
+static bool raiser_pressed = false;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+    case LOWER:
+      if (record->event.pressed) {
+        lower_pressed = true; // (2)
+
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+
+        if (lower_pressed) { // (4)
+          register_code(KC_LANG2); // macOS の場合は KC_LANG2
+          unregister_code(KC_LANG2);
+        }
+        lower_pressed = false;
+      }
+      return false; // (5)
+      break;
+
+    case RAISE:
+      if (record->event.pressed) {
+        raiser_pressed = true; // (2)
+
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+
+        if (raiser_pressed) { // (4)
+          register_code(KC_LANG1); // macOS の場合は KC_LANG2
+          unregister_code(KC_LANG1);
+        }
+        raiser_pressed = false;
+      }
+      return false; // (5)
+      break;
+
     case QWERTY:
       if (record->event.pressed) {
         set_single_persistent_default_layer(_QWERTY);
       }
       return false;
       break;
-    case COLEMAK:
+
+    default: // (3)
       if (record->event.pressed) {
-        set_single_persistent_default_layer(_COLEMAK);
+        // reset the flag
+        lower_pressed = false;
+        raiser_pressed = false;
       }
-      return false;
       break;
-    case DVORAK:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_DVORAK);
-      }
-      return false;
-      break;
+
   }
   return true;
 }
