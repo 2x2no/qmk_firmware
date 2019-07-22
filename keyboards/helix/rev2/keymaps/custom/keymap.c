2b2496b319a1b1a75b75c47df673b12361197f9e
shinomiya <shinomiya@synchro-food.co.jp>
2019-07-22 13:36:58 +0900

    Helix 右手記号の修正

--- keyboards/helix/rev2/keymaps/custom/keymap.c
+++ keyboards/helix/rev2/keymaps/custom/keymap.c
@@ -65,22 +65,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
                 * ,-----------------------------------------.             ,-----------------------------------------.
-                * |   `  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  |  =   |
+                * |   `  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  |  \   |
                 * |------+------+------+------+------+------|             |------+------+------+------+------+------|
                 * | Tab  |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  |  -   |
                 * |------+------+------+------+------+------|             |------+------+------+------+------+------|
                 * | Ctrl |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  '   |
                 * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-                * | Shift|   Z  |   X  |   C  |   V  |   B  |  [   |  ]   |   N  |   M  |   ,  |   .  |   /  |  [   |
+                * | Shift|   Z  |   X  |   C  |   V  |   B  |  [   |  ]   |   N  |   M  |   ,  |   .  |   /  |  =   |
                 * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
                 * | Esc  |LOWER | Ctrl |  Alt | LGUI | Space| Del  | Bksp | Enter| Raise| Left | Down |  Up  |Right |
                 * `-------------------------------------------------------------------------------------------------'
                 */
   [_QWERTY] = LAYOUT( \
-      KC_GRV,  KC_1,  KC_2,    KC_3,    KC_4,            KC_5,                      KC_6,    KC_7,      KC_8,    KC_9,    KC_0,    KC_EQL, \
+      KC_GRV,  KC_1,  KC_2,    KC_3,    KC_4,            KC_5,                      KC_6,    KC_7,      KC_8,    KC_9,    KC_0,    KC_BSLS, \
       KC_TAB,  KC_Q,  KC_W,    KC_E,    KC_R,            KC_T,                      KC_Y,    KC_U,      KC_I,    KC_O,    KC_P,    KC_MINS, \
       KC_LCTL, KC_A,  KC_S,    KC_D,    KC_F,            KC_G,                      KC_H,    KC_J,      KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-      KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_V,            KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_LBRC , \
+      KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_V,            KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_EQL , \
       KC_ESC,  LOWER, KC_LCTL, KC_LGUI, ALT_T(KC_LANG2), KC_SPC,  KC_DEL,  KC_BSPC, KC_ENT,  KC_RASEKN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),
 
